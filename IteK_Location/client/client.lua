b402e311f173568a0ccb6fa2988a7cd0014c31ea
Cuchi <jusaumsp@gmail.com>
2022-03-08 19:03:57 +0100

    refactor(core): cleanup

--- IteK_Location/client/client.lua
+++ IteK_Location/client/client.lua
@@ -1,12 +1,11 @@
-Citizen.CreateThread(function()
+CreateThread(function()
     while ESX == nil do
         TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-        Citizen.Wait(5000)
+        Wait(5000)
     end
 end)
 
 -- MENU
-
 local mainMenu = RageUI.CreateMenu("Location", "MENU")
 local voiture = RageUI.CreateSubMenu(mainMenu, "Voitures", "MENU")
 local moto = RageUI.CreateSubMenu(mainMenu, "Vélos", "MENU")
@@ -19,19 +18,21 @@ moto.Display.Glare = true
 mainMenu.Closed = function() open = false end
 
 function location()
-    if open then 
-        open = false 
-            RageUI.Visible(mainMenu, false)
-        return 
-    else 
-        open = true 
-            RageUI.Visible(mainMenu, true)
-        Citizen.CreateThread(function()
+    if open then
+        open = false
+        RageUI.Visible(mainMenu, false)
+        return
+    else
+        open = true
+        RageUI.Visible(mainMenu, true)
+
+        CreateThread(function()
             while open do
                 RageUI.IsVisible(mainMenu, function()
                     RageUI.Button("Voitures", nil, {RightLabel = "→"}, true, {onActive = function() end}, voiture)
                     RageUI.Button("Vélos", nil, {RightLabel = "→"}, true, {onActive = function() end}, moto)
                 end)
+
                 RageUI.IsVisible(voiture, function()
                     RageUI.Button("Asbo", nil, {RightLabel = config.prixvoiture1}, true, {
                         onSelected = function()
@@ -48,6 +49,7 @@ function location()
                         end
                     })
                 end)
+
                 RageUI.IsVisible(moto, function()
                     RageUI.Button("BMX", nil, {RightLabel = config.prixmoto1}, true, {
                         onSelected = function()
@@ -57,17 +59,19 @@ function location()
                         end
                     })
                 end)
-            Wait(0)
+
+                Wait(0)
             end
         end)
     end
 end
 
-Citizen.CreateThread(function()
+CreateThread(function()
     while true do
-      local wait = 900
-        for k,v in pairs(Config.position) do
-            local plyCoords = GetEntityCoords(GetPlayerPed(-1), false)
+        local wait = 900
+
+        for _,v in pairs(Config.position) do
+            local plyCoords = GetEntityCoords(PlayerPedId(), false)
             local dist = #(plyCoords - v)
             if dist <= 1.8 then
                 wait = 1
@@ -75,37 +79,36 @@ Citizen.CreateThread(function()
                 if IsControlJustPressed(1,51) then
                     location()
                 end
-            end 
+            end
         end
-    Citizen.Wait(wait)
+
+        Wait(wait)
     end
 end)
 
 -- BLIPS
- 
-Citizen.CreateThread(function() 
-    for k, v in pairs(Config.position) do 
+CreateThread(function()
+    for _, v in pairs(Config.position) do
         local blip = AddBlipForCoord(v.x, v.y, v.z)
-            SetBlipSprite  (blip, config.blipsprite)
-            SetBlipDisplay (blip, config.blipdisplay)
-            SetBlipScale   (blip, config.blipscale)
-            SetBlipColour  (blip, config.blipcolour)
-            SetBlipAsShortRange(blip, true) 
-            BeginTextCommandSetBlipName('STRING') 
-            AddTextComponentSubstringPlayerName(config.blipname)
-            EndTextCommandSetBlipName(blip) 
-    end 
+        SetBlipSprite(blip, config.blipsprite)
+        SetBlipDisplay(blip, config.blipdisplay)
+        SetBlipScale(blip, config.blipscale)
+        SetBlipColour(blip, config.blipcolour)
+        SetBlipAsShortRange(blip, true)
+        BeginTextCommandSetBlipName('STRING')
+        AddTextComponentSubstringPlayerName(config.blipname)
+        EndTextCommandSetBlipName(blip)
+    end
 end)
 
 -- PED
-
-Citizen.CreateThread(function()
+CreateThread(function()
     local hash = GetHashKey("g_m_m_korboss_01")
     while not HasModelLoaded(hash) do
-    RequestModel(hash)
-    Wait(20)
+        RequestModel(hash)
+        Wait(20)
     end
-    ped = CreatePed("PED_TYPE_CIVFEMALE", "g_m_m_korboss_01", -283.4137, -888.6544, 30.0806, 79.9023, false, true)
+    local ped = CreatePed("PED_TYPE_CIVFEMALE", "g_m_m_korboss_01", -283.4137, -888.6544, 30.0806, 79.9023, false, true)
     SetBlockingOfNonTemporaryEvents(ped, 1)
     FreezeEntityPosition(ped, 1)
     SetEntityInvincible(ped, 1)
--- IteK_Location/server/server.lua
+++ IteK_Location/server/server.lua
@@ -1,45 +1,50 @@
 ESX = nil
-
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
- 
+
 RegisterNetEvent('IteK:achatAsbo')
 AddEventHandler('IteK:achatAsbo', function()
-  local xPlayer = ESX.GetPlayerFromId(source)  
-  local prix = 250
-  local argent = xPlayer.getMoney()
-    if argent >= prix then
-      xPlayer.removeMoney(prix)
-      SetVehicleNumberPlateText(CreateVehicle(GetHashKey("asbo"), -309.3480, -897.6269, 31.0806, 347.9820, true, false), "LOCATION")
-      TriggerClientEvent('esx:showNotification', source, "Achat effectué !")
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    local price = 250
+    local money = xPlayer.getMoney()
+
+    if money >= prix then
+        xPlayer.removeMoney(price)
+        SetVehicleNumberPlateText(CreateVehicle(GetHashKey("asbo"), -309.3480, -897.6269, 31.0806, 347.9820, true, false), "LOCATION")
+        TriggerClientEvent('esx:showNotification', _source, "Achat effectué !")
     else
-      TriggerClientEvent('esx:showNotification', source, "Vous n'avez pas assez d\'argent")
+        TriggerClientEvent('esx:showNotification', _source, "Vous n'avez pas assez d\'argent")
     end
 end)
 
 RegisterNetEvent('IteK:achatClub')
 AddEventHandler('IteK:achatClub', function()
-  local xPlayer = ESX.GetPlayerFromId(source)  
-  local prix = 125
-  local argent = xPlayer.getMoney()
-    if argent >= prix then
-      xPlayer.removeMoney(prix)
-      SetVehicleNumberPlateText(CreateVehicle(GetHashKey("club"), -312.9246, -896.8875, 31.0752, 347.5302, true, false), "LOCATION")
-      TriggerClientEvent('esx:showNotification', source, "Achat effectué !")
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local price = 125
+    local money = xPlayer.getMoney()
+
+    if money >= price then
+        xPlayer.removeMoney(price)
+        SetVehicleNumberPlateText(CreateVehicle(GetHashKey("club"), -312.9246, -896.8875, 31.0752, 347.5302, true, false), "LOCATION")
+        TriggerClientEvent('esx:showNotification', _source, "Achat effectué !")
     else
-      TriggerClientEvent('esx:showNotification', source, "Vous n'avez pas assez d\'argent")
+        TriggerClientEvent('esx:showNotification', _source, "Vous n'avez pas assez d\'argent")
     end
 end)
 
 RegisterNetEvent('IteK:achatBMX')
 AddEventHandler('IteK:achatBMX', function()
-  local xPlayer = ESX.GetPlayerFromId(source)  
-  local prix = 25
-  local argent = xPlayer.getMoney()
-    if argent >= prix then
-      xPlayer.removeMoney(prix)
-      SetVehicleNumberPlateText(CreateVehicle(GetHashKey("bmx"), -285.7448, -887.2358, 31.0806, 171.4600, true, false), "LOCATION")
-      TriggerClientEvent('esx:showNotification', source, "Achat effectué !")
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local price = 25
+    local money = xPlayer.getMoney()
+
+    if money >= price then
+        xPlayer.removeMoney(price)
+        SetVehicleNumberPlateText(CreateVehicle(GetHashKey("bmx"), -285.7448, -887.2358, 31.0806, 171.4600, true, false), "LOCATION")
+        TriggerClientEvent('esx:showNotification', _source, "Achat effectué !")
     else
-      TriggerClientEvent('esx:showNotification', source, "Vous n'avez pas assez d\'argent")
+        TriggerClientEvent('esx:showNotification', _source, "Vous n'avez pas assez d\'argent")
     end
 end)
